0d981434f531045fbb6e9ea186314e7abfd0c1f9
jeki <jq_rbp@yahoo.com>
2021-04-22 17:45:15 +0900

    feat: refine getLocation

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:adhan/adhan.dart';
 import 'package:location/location.dart';
 import 'package:intl/intl.dart';
+import 'package:permission_handler/permission_handler.dart' as permHandler;
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -12,17 +15,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Adhan',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Jam Sholat'),
@@ -33,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,20 +34,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _prayerNames = [
-    '-',
-    'Subuh',
-    'Pagi',
-    'Dhuhur',
-    'Ashar',
-    'Maghrib',
-    'Isya'
-  ];
+  Coordinates _myCoordinates = Coordinates(37.5371364, 127.0832429);
 
-  Qibla _qibla;
-  Prayer _currPrayer;
-  Prayer _nextPrayer;
-  LocationData _locationData;
+  @override
+  void initState() {
+    super.initState();
+    _getCoordinates().then((locationData) {
+      setState(() {
+        if (locationData != null) {
+          _myCoordinates =
+              Coordinates(locationData.latitude, locationData.longitude);
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,71 +71,61 @@ class _MyHomePageState extends State<MyHomePage> {
       body: FutureBuilder(
           future: _generatePrayerTimes(),
           builder: (BuildContext context, AsyncSnapshot<PrayerTimes> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    '${snapshot.error} occured',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                );
-              } else if (snapshot.hasData) {
-                return ListView(
-                  children: [
-                    // ListTile(
-                    //     title: Text('Time For Prayer'),
-                    //     subtitle: Text(DateFormat.jm().format(snapshot.data
-                    //         .timeForPrayer(snapshot.data.currentPrayer())))),
-                    _genPrayerListTile(snapshot.data, Prayer.fajr),
-                    ListTile(
-                      title: Text('Pagi'),
-                      subtitle: Text(
-                        DateFormat.jm().format(snapshot.data.sunrise),
-                      ),
+            // if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '${snapshot.error} occured',
+                  style: TextStyle(fontSize: 18),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              return ListView(
+                children: [
+                  _genPrayerListTile(snapshot.data, Prayer.fajr),
+                  ListTile(
+                    title: Text('Pagi'),
+                    subtitle: Text(
+                      DateFormat.jm().format(snapshot.data.sunrise),
                     ),
-                    _genPrayerListTile(snapshot.data, Prayer.dhuhr),
-                    _genPrayerListTile(snapshot.data, Prayer.asr),
-                    _genPrayerListTile(snapshot.data, Prayer.maghrib),
-                    _genPrayerListTile(snapshot.data, Prayer.isha),
-                    ListTile(
-                        title: Text('Qiyam'),
-                        subtitle: Text(DateFormat.jm().format(
-                            SunnahTimes(snapshot.data).lastThirdOfTheNight))),
-                    // ListTile(
-                    //     title: Text('Current Prayer'),
-                    //     subtitle:
-                    //         Text(snapshot.data.currentPrayer().toString())),
-                    // ListTile(
-                    //     title: Text('Next Prayer'),
-                    //     subtitle: Text(snapshot.data.nextPrayer().toString())),
-                    ListTile(
-                        title: Text('Qibla'),
-                        subtitle: Text(_qibla.direction.toString())),
-                    // ListTile(
-                    //   title: Text('Location'),
-                    //   subtitle: Text('Latitude: ' +
-                    //       _locationData.latitude.toString() +
-                    //       ', Longitude: ' +
-                    //       _locationData.longitude.toString()),
-                    // ),
-                  ],
-                );
-              }
-            } else if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: Text('Loading'));
+                  ),
+                  _genPrayerListTile(snapshot.data, Prayer.dhuhr),
+                  _genPrayerListTile(snapshot.data, Prayer.asr),
+                  _genPrayerListTile(snapshot.data, Prayer.maghrib),
+                  _genPrayerListTile(snapshot.data, Prayer.isha),
+                  ListTile(
+                    title: Text('Qiyam'),
+                    subtitle: Text(
+                      DateFormat.jm().format(
+                          SunnahTimes(snapshot.data).lastThirdOfTheNight),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Arah Kiblat'),
+                    subtitle: Text(
+                      Qibla(_myCoordinates).direction.toStringAsFixed(2),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('Lokasi'),
+                    subtitle: Text(
+                      _myCoordinates.latitude.toString() +
+                          ', ' +
+                          _myCoordinates.longitude.toString(),
+                    ),
+                  ),
+                ],
+              );
             }
-            return Center(child: Text('Loading'));
+            // } else if (snapshot.connectionState == ConnectionState.waiting) {
+            //   return Center(child: Text('Loading'),);
+            // }
+            return Center(
+              child: Text('Loading'),
+            );
           }),
     );
   }
-
-  // _genCalcRemainingTime(PrayerTimes _prayerTimes, Prayer _prayer) {
-  //   var currPrayer = _prayerTimes.currentPrayer();
-  //   if (_prayer == currPrayer) {
-
-  //     return
-  //   }
-  // }
 
   DateTime _getPrayerTime(PrayerTimes _prayerTimes, Prayer _prayer) {
     switch (_prayer) {
@@ -170,9 +145,24 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   ListTile _genPrayerListTile(PrayerTimes _prayerTimes, Prayer _prayer) {
+    List<String> _prayerNames = [
+      '-',
+      'Subuh',
+      'Pagi',
+      'Dhuhur',
+      'Ashar',
+      'Maghrib',
+      'Isya'
+    ];
+
     DateTime _prayerDateTime = _getPrayerTime(_prayerTimes, _prayer);
+
+    Prayer _currPrayer = _prayerTimes.currentPrayer();
+    Prayer _nextPrayer = _prayerTimes.nextPrayer();
+
     bool _currPrayerFlag = _currPrayer == _prayer;
     bool _nextPrayerFlag = _nextPrayer == _prayer;
+
     Color _textColor = _currPrayerFlag
         ? Colors.blue
         : _nextPrayerFlag
@@ -191,7 +181,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       trailing: (_currPrayerFlag || _nextPrayerFlag)
           ? Text(
-              _getMinuteDiff(_prayerDateTime).toString() + " Minutes",
+              _getMinuteDiff(_prayerDateTime).toString() + " Menit",
               style: TextStyle(fontSize: 18, color: _textColor),
             )
           : Text(""),
@@ -203,61 +193,82 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<PrayerTimes> _generatePrayerTimes() async {
-    _locationData = await _getCoordinates();
-    Coordinates _myCoordinates = Coordinates(37.5371364, 127.0832429);
-    if (_locationData != null) {
-      _myCoordinates =
-          Coordinates(_locationData.latitude, _locationData.longitude);
-    }
     // Replace with your own location lat, lng.
     final params = CalculationMethod.muslim_world_league.getParameters();
     params.madhab = Madhab.shafi;
-    final snackBar = SnackBar(
-      content: Text('Latitude: ' +
-          _locationData.latitude.toString() +
-          ', Longitude: ' +
-          _locationData.longitude.toString()),
-    );
-
-    // Find the ScaffoldMessenger in the widget tree
-    // and use it to show a SnackBar.
-    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
-    _qibla = Qibla(_myCoordinates);
-
     var _prayerTimes = PrayerTimes.today(_myCoordinates, params);
-    _currPrayer = _prayerTimes.currentPrayer();
-    _nextPrayer = _prayerTimes.nextPrayer();
+
     return _prayerTimes;
   }
 
-  _refreshPrayer() {
-    _generatePrayerTimes();
+  _refreshPrayer() async {
+    LocationData locationData = await _getCoordinates();
+    if (locationData != null) {
+      final snackBar = SnackBar(
+        content: Text(
+          'Latitude: ' +
+              locationData.latitude.toString() +
+              ', Longitude: ' +
+              locationData.longitude.toString(),
+        ),
+      );
+
+      // Find the ScaffoldMessenger in the widget tree
+      // and use it to show a SnackBar.
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      setState(() {
+        _myCoordinates =
+            Coordinates(locationData.latitude, locationData.longitude);
+      });
+    }
+  }
+
+  _requestPermision() async {
+    Map<permHandler.Permission, permHandler.PermissionStatus> statuses = await [
+      permHandler.Permission.location,
+    ].request();
+    // print(statuses[permHandler.Permission.location]);
+    return statuses;
   }
 
   Future<LocationData> _getCoordinates() async {
     Location location = new Location();
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-    LocationData _locationData;
+    // bool _serviceEnabled;
+    // PermissionStatus _permissionGranted;
+    LocationData locationData;
 
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return null;
-      }
+    var statuses = await _requestPermision();
+
+    if (statuses[permHandler.Permission.location] !=
+        permHandler.PermissionStatus.granted) {
+      return null;
     }
 
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return null;
-      }
-    }
-    _locationData = await location.getLocation();
+    // _serviceEnabled = await location.serviceEnabled();
+    // if (!_serviceEnabled) {
+    //   _serviceEnabled = await location.requestService();
+    //   if (!_serviceEnabled) {
+    //     return null;
+    //   }
+    // }
 
-    return _locationData;
+    // _permissionGranted = await location.hasPermission();
+    // if (_permissionGranted == PermissionStatus.denied) {
+    //   _permissionGranted = await location.requestPermission();
+    //   if (_permissionGranted != PermissionStatus.granted) {
+    //     return null;
+    //   }
+    // }
+
+    locationData = await location.getLocation();
+
+    // location.onLocationChanged.listen((LocationData currentLocation) {
+    //   // Use current location
+    //   setState(() {
+    //     _myCoordinates =
+    //         Coordinates(locationData.latitude, locationData.longitude);
+    //   });
+    // });
+    return locationData;
   }
 }
